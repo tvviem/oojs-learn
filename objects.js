8607f872e581881254e82c6241b8320323ce65b2
viem trieu <trieuvinhviem@gmail.com>
2018-06-14 09:14:17 +0700

    method login/logout in User class

--- objects.js
+++ objects.js
@@ -1,10 +1,16 @@
-var User = {
-    email: 'trieuvinhviem@gmail.com',
-    name: 'Trieu vinh viem',
-    login: function() {
-        console.log(this.email, 'has logged in');
-    },
-    logout: function() {
-        console.log(this.email, 'has logged out');
+// Define a class
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
     }
-};
+    login() {
+        console.log(this.email, 'just logged in');
+    }
+    logout() {
+        console.log(this.email, 'just logged out');
+    }
+}
+// create user objects
+var user1 = new User('tvviem@blu.edu.vn', 'Viem Trieu');
+var user2 = new User('doublefire@gmail.com', 'viem');
